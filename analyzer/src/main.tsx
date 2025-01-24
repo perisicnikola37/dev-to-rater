6d2bcae7d89dc35eba630c02ff83e2bb3d4a0cb5
Nikola <nikola.perisic@vegait.rs>
2025-01-24 11:50:56 +0100

    feat: configure sass for styles

--- analyzer/src/main.tsx
+++ analyzer/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./styles/main.css";
 import DevToPostAnalyzer from "./DevToPostAnalyzer.tsx";
 
 createRoot(document.getElementById("root")!).render(<DevToPostAnalyzer />);
