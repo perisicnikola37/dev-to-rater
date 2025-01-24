d4bde7a2fe7ae62a5e63d58f6090903055c0c14c
Nikola Perisic <perisicnikola37@gmail.com>
2025-01-24 17:10:41 +0100

    refactor: modify calculateHeadingStructureScore.ts file

--- analyzer/src/core/calculateHeadingStructureScore.ts
+++ analyzer/src/core/calculateHeadingStructureScore.ts
@@ -13,9 +13,6 @@ export const calculateHeadingStructureScore = (
   if (charactersCount > 5000) {
     penalty += 2 // If there are more than 5000 characters, add a penalty
   }
-
   // TODO: Maybe add bonus point if below 1500 characters
-
-  console.log(`Characters count: ${charactersCount}, Penalty: ${penalty}`)
   return penalty
 }
