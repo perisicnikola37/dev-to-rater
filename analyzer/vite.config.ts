f4b828bb7e064276ed2613cad27d4a56a96e3994
Nikola <nikola.perisic@vegait.rs>
2025-01-28 12:11:43 +0100

    feat: add vite compress plugin

--- analyzer/vite.config.ts
+++ analyzer/vite.config.ts
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import ViteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    ViteImagemin({
+      gifsicle: {
+        optimizationLevel: 3,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      pngquant: {
+        quality: [0.65, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            removeViewBox: false,
+          },
+        ],
+      },
+      webp: {
+        quality: 75,
+      },
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
