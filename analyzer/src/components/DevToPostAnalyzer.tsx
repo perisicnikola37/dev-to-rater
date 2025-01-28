ea6c94b7097664d2c61a4d7b12c0184c3cf3c719
Nikola <nikola.perisic@vegait.rs>
2025-01-28 08:33:57 +0100

    feat: add personalized email address

--- analyzer/src/components/DevToPostAnalyzer.tsx
+++ analyzer/src/components/DevToPostAnalyzer.tsx
@@ -65,6 +65,25 @@ const DevToPostAnalyzer: React.FC = () => {
       <div className="min-h-screen flex items-start justify-center mt-16 mb-16">
         <div className="w-full max-w-3xl rounded-3xl h-full flex flex-col items-center">
           <Header />
+          <div className="absolute top-5 right-20 flex items-center space-x-4">
+            <a
+              className="border-b border-transparent hover:border-b-1 hover:border-b-blue-500 duration-200"
+              href="https://docs.dev-to-rater.xyz"
+              target="_blank"
+            >
+              docs
+            </a>
+            <a
+              href="https://github.com/perisicnikola37/dev-to-rater"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                alt="GitHub Repo stars"
+                src="https://img.shields.io/github/stars/perisicnikola37/dev-to-rater"
+              />
+            </a>
+          </div>
           <URLForm
             inputURL={inputURL}
             setInputURL={setInputURL}
