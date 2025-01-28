ea256abcb14dc2e46678c74a857c48845c74fd70
Nikola <nikola.perisic@vegait.rs>
2025-01-28 14:09:38 +0100

    feat: add hover on radar chart

--- analyzer/src/components/RadarChart.tsx
+++ analyzer/src/components/RadarChart.tsx
@@ -5,23 +5,36 @@ import {
   Radar,
   ResponsiveContainer,
   RadarChart,
+  Tooltip,
 } from 'recharts'
 import { RadarComponentProps } from '../interfaces/props/RadarComponentProps'
 
 const RadarComponent: React.FC<RadarComponentProps> = ({ data }) => {
   return (
-    <ResponsiveContainer width="100%" height={350}>
-      <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
+    <ResponsiveContainer className="select-none" width="100%" height={350}>
+      <RadarChart
+        cx="50%"
+        cy="50%"
+        outerRadius="80%"
+        width={800}
+        height={500}
+        data={data}
+      >
         <PolarGrid />
         <PolarAngleAxis dataKey="subject" />
-        <PolarRadiusAxis />
+        <PolarRadiusAxis
+          angle={30}
+          domain={[0, Math.max(...data.map((d) => d.fullMark))]}
+        />
         <Radar
-          name="Content"
+          name="Total"
           dataKey="A"
           stroke="#4FD6FF"
           fill="#4FD6FF"
           fillOpacity={0.6}
+          isAnimationActive={true}
         />
+        <Tooltip />
       </RadarChart>
     </ResponsiveContainer>
   )
