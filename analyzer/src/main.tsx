e15fd05a035587f10872b4cca2edee7233e86755
Nikola <nikola.perisic@vegait.rs>
2025-01-30 07:17:31 +0100

    refactor: add ga4 library

--- analyzer/src/main.tsx
+++ analyzer/src/main.tsx
@@ -2,5 +2,8 @@ import { createRoot } from 'react-dom/client'
 import '@/styles/tailwind.css'
 import '@/styles/main.css'
 import DevToPostAnalyzer from '@/pages/DevToPostAnalyzer'
+import ReactGA from 'react-ga4'
+
+ReactGA.initialize('G-P6JEX3V8ST')
 
 createRoot(document.getElementById('root')!).render(<DevToPostAnalyzer />)
